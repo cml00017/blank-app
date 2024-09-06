0c8827c3eaf08e6f2bcff86ca7381c28cf86fb1f
Connor Lewis <104527461+cml00017@users.noreply.github.com>
2024-09-06 14:09:03 +0000

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+import random
+
+# List of names
+names = ["Caleb", "Nya", "Connor", "Elliot", "Morgan"]
+
+# Set the title of the app
+st.title("Random Name Selector")
+
+# Add a button to trigger the random selection
+if st.button("Select a Random Name"):
+    # Randomly select a name
+    selected_name = random.choice(names)
+    # Display the selected name
+    st.write(f"The randomly selected name is: **{selected_name}**")
+else:
+    st.write("Click the button to select a random name!")
+
